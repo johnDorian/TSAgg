967c215cae0d12df4352030335fd13ab91ffee8d
Jason Lessels <jason@jason-desktop.(none)>
2009-12-03 16:16:06 +1100

    Fixed up the startup script and the hour.agg function

--- R/zzz.R
+++ R/zzz.R
@@ -1,22 +1,12 @@
-".First.lib" <- function(lib, pkg)
+.First.lib <- function(lib, pkg)
 {
-  library.dynam("TSAgg", package = pkg, lib.loc = lib)
-  messages <- as.logical(ifelse(is.null(getOption("TSAgg.messages")),
-                                TRUE, getOption("TSAgg.messages")))
-  if(messages){
-    cat("\n")
-    cat("-------------------------------------------------------------\n")
-    pkg.info <- drop(read.dcf(file=system.file("DESCRIPTION",
-                                package="TSAgg"),
-                              fields=c("Title","Version","Date")))
-    cat(pkg.info["Title"])
-    cat("\n")
-    cat("This package is designed to make time series aggregation easy.")
-    cat(paste("TSAgg version ", pkg.info["Version"],
-              " (built on ", pkg.info["Date"], ") is now loaded\n", sep=""))
-cat(paste("For more infomation or to report issues please contact",pkg.info["Maintainer"]))
-    cat("-------------------------------------------------------------\n")
-    cat("\n")
-  }
-  return(invisible(0))
+cat("------------------------------------------------")
+cat("\n")
+cat("This package is still under developement.")
+cat("\n")
+cat("Please use with caution.")
+cat("\n")
+cat("------------------------------------------------")
+cat("\n")
 }
+
