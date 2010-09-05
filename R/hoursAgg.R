581ffb3ac1bd60afc5be74688adcfa29bee0502b
Jason Lessels <jlessels@gmail.com>
2010-09-05 21:56:06 +1000

    Fixed up the function, atually works now

--- R/hoursAgg.R
+++ R/hoursAgg.R
@@ -1,37 +1,15 @@
-hoursAgg<-
-	function(data,
-			process,
-			multiple=NULL,
-			na.rm=FALSE,
-			tz="GMT"){
-
-		
-          if(!is.null(multiple)){
-          	if(multiple>1){
-            dates <- as.POSIXct(strptime(paste(data$hour,data$day,data$month,data$year),"%H %d %m %Y"),tz=tz)
-            ## Get the gep between each hour.
-            gap <- as.numeric(difftime(dates[2:length(dates)],dates[1:length(dates)-1]),units="hours")
-            cum.gap <- gap
-            for(i in 2:length(cum.gap)){
-              cum.gap[i]=cum.gap[i-1]+cum.gap[i]+data$hour[1]
-            }
-            cum.gap <- c(data$hour[1],cum.gap)
-            data$hour<-cum.gap-cum.gap%%multiple-cum.gap[1]
-            #Convert the above hours from date[1] to dates.
-           data$dates <- data$dates[1]+data$hour*60*60
-           
-             temp <- timeSeries(data$dates,"%Y-%m-%d %H:%M:%S",data[,8:length(data)],tz=tz)
-            data <- temp
-            }else{
-            	data <- data
-            	        
-          }}
-          
-          result<-aggregate(data[,8:length(data)],list(day=data$day,month=data$month,year=data$year,hour=data$hour),process,na.rm=na.rm)
-
-         data2 <- data.frame(Date=strptime(paste(00,result$hour,result$day,result$month,result$year),"%M %H %d %m %Y",tz=tz),result[,5:length(result)])
-        sorted <- data2[order(data2$Date),]
-        final <- data.frame(Date=as.POSIXlt(sorted$Date),sorted[,2:length(sorted)])
-        names(final)<-c("Date",names(data[8:length(data)]))
-          return(final)
-        }
+"hoursAgg"<-function 
+(data, process, multiple = 1, na.rm = FALSE, tz = "GMT") 
+{
+	gap <- as.numeric(difftime(data$dates,data$dates[1],tz="GMT", units = "hours"))
+	agg.gap<-gap-(gap%%multiple)
+	data$dates<-data$dates[1] + agg.gap * 60 * 60
+	data <- timeSeries(data$dates, "%Y-%m-%d %H:%M:%S",data[, 8:length(data)], tz = tz)
+	result <- aggregate(data[, 8:length(data)], list(day = data$day,month = data$month, year = data$year, hour = data$hour,minute=data$minute),process, na.rm = na.rm)
+    data2 <- data.frame(Date = strptime(paste(result$minute, result$hour,result$day, result$month, result$year), "%M %H %d %m %Y",tz = tz), result[, 6:length(result)])
+    sorted <- data2[order(data2$Date), ]
+    final <- data.frame(Date = as.POSIXlt(sorted$Date), sorted[, 
+        2:length(sorted)])
+    names(final) <- c("Date", names(data[8:length(data)]))
+    return(final)
+}
