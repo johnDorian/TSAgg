74d59201304704602337814e6eaf4c2ad2fb4670
JohnDorian <jasonlessels@Macintosh-2.local>
2010-09-05 21:53:50 +1000

    Fixed up the error checking

--- R/timeSeries.R
+++ R/timeSeries.R
@@ -1,31 +1,31 @@
-timeSeries<-
-	function(dates,
-			 dateformat,
-			 data=NULL,
-			 tz="GMT"){
-		if(!is.null(data)&&class(as.vector(data))!="numeric")stop("Data must be numeric")		
-		if(class(data)=="numeric")data.length=length(data)else data.length=length(data[,1])
-		if(!is.null(data)&&length(dates)!=data.length)
-			stop("Lengths differ between dates (",length(dates),") and data (",data.length, ").")
-			
-		dates<-(strptime(paste(dates),dateformat,tz=tz))
-		minute<- as.numeric(format(dates,"%M"))
-		hour<- as.numeric(format(dates,"%H"))
-		day<-as.numeric(format(dates,"%d"))
-		week<-as.numeric(format(dates,"%W"))
-		month<-as.numeric(format(dates,"%m"))
-		year<-as.numeric(format(dates,"%Y"))
-
-		
-		if(is.null(data)){	
-			results<-data.frame(dates,minute,hour,day,week,month,year)
+"timeSeries"<-function
+(dates, dateformat, data = NULL, tz = "GMT") 
+{
+	dimensions<-dim(data)
+	clss<-lapply(data,class)
+	if (!is.null(data)){
+		if (!is.null(dimensions[1])){
+			if (length(dates)!=length(data[,1]))
+				stop("Data and Date lengths differ")
 		}else{
-			results<-data.frame(dates,minute,hour,day,week,month,year,data)
+			if (length(dates)!=length(data))
+				stop("Data and Date lengths differ")
 		}
-			return(results)
-		
-	}
-
-
-
-
+	}	
+    dates <- (strptime(paste(dates), dateformat, tz = tz))
+    minute <- as.numeric(format(dates, "%M"))
+    hour <- as.numeric(format(dates, "%H"))
+    day <- as.numeric(format(dates, "%d"))
+    week <- as.numeric(format(dates, "%W"))
+    month <- as.numeric(format(dates, "%m"))
+    year <- as.numeric(format(dates, "%Y"))
+    if (is.null(data)) {
+        results <- data.frame(dates, minute, hour, day, week, 
+            month, year)
+    }
+    else {
+        results <- data.frame(dates, minute, hour, day, week, 
+            month, year, data)
+    }
+    return(results)
+}
